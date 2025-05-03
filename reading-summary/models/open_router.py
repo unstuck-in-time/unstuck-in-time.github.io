78c80b8355a39d7f637a7b38d5a1ca36807d02ac
ethanbg2 <ethanbg2@illinois.edu>
2025-05-03 18:15:21 -0400

    add python module for reading summary

---
+++ reading-summary/models/open_router.py
@@ -0,0 +1,20 @@
+from langchain_community.chat_models import ChatOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class ChatOpenRouter(ChatOpenAI):
+    openai_api_base: str
+    openai_api_key: str
+    model_name: str
+
+    def __init__(self,
+                 model_name: str,
+                 openai_api_key: str = None,
+                 openai_api_base: str = "https://openrouter.ai/api/v1",
+                 **kwargs):
+        openai_api_key = openai_api_key or os.getenv('OPENROUTER_API_KEY')
+        super().__init__(openai_api_base=openai_api_base,
+                         openai_api_key=openai_api_key,
+                         model_name=model_name, **kwargs)
