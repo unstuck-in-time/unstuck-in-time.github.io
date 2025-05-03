535fd24a05556f68d95631fdae3c577307ab888e
ethanbg2 <ethanbg2@illinois.edu>
2025-05-03 00:58:09 -0400

    got rid of projects

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,6 @@ export const SITE: Site = {
   DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
   EMAIL: "trevortylerlee@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
@@ -16,12 +15,6 @@ export const HOME: Metadata = {
 export const BLOG: Metadata = {
   TITLE: "Blog",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
-
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
 };
 
 export const SOCIALS: Socials = [
