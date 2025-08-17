6270a9f35d1a814d60d80a1fa280ab1917f27e35
ethanbg2 <ethanbg2@illinois.edu>
2025-08-16 20:57:54 -0400

    experiment with pdf export

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,34 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+// import pdf from "astro-pdf";
+
+// const pdfOptions = {
+//   pages: (pathname) => {
+//     if (pathname.startsWith("/blog/")) {
+//       return {
+//         throwOnFail: true,
+//         path: pathname.substring(0, pathname.length - 1) + '.pdf',
+//         pdf: {
+//           displayHeaderFooter: false,
+//           outline: true,
+//           scale: 0.9,
+//           margin: {
+//             top: "20mm",
+//             bottom: "20mm",
+//             left: "15mm",
+//             right: "15mm",
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://unstuck-in-time.github.io",
-  integrations: [sitemap(), mdx(), pagefind(), react()],
+  integrations: [sitemap(), mdx(), pagefind(), react()], // pdf(pdfOptions)
   vite: {
     plugins: [tailwindcss()],
   },
