82966e664a7864967d1829110e098eca3af04995
ethanbg2 <ethanbg2@illinois.edu>
2025-08-16 23:57:27 -0400

    update uncertainty article

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,10 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
+  loader: glob({
+    pattern: '**/!(_*)/[^_]*.{md,mdx}',
+    base: "./src/content/blog"
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
